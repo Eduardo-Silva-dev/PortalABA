df7353b11f9c8ac87bb321e43d67ae7edd6d2b86
Eduardo-Silva-dev <ads.eduardo.silva@gmail.com>
2020-03-30 10:53:56 -0300

    Configuração do Cors e alteração do db

--- PortalAba/src/main/java/config/ConfigurationCors.java
+++ PortalAba/src/main/java/config/ConfigurationCors.java
@@ -1,4 +1,4 @@
-package com.portalaba.apirest;
+package config;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
---
+++ PortalAba/src/main/java/config/securityConfig.java
@@ -0,0 +1,26 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity (prePostEnabled = true)
+public class securityConfig {
+	protected void configure (HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.httpBasic()
+			.and()
+			.csrf().disable();
+	}
+	
+	@Autowired
+	public void condigureGlobal (AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("admin").password("admin").roles("ADMIN");
+	}
+}
